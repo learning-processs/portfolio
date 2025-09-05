eedc0e56e19cdbdfbda90bf105455b8f7fb5169d
Anu025 <anu001125@gmail.com>
2025-09-05 08:22:37 +0530

    some update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="flex justify-between bg-gray-900  text-gray-400 px-4 py-4">
         <div className="max-sm:hidden font-semibold cursor-pointer">myPortfolio</div>
         <div className="">
-          <ul className="w-full flex justify-evenly gap-20 cursor-pointer  ">
+          <ul className="w-full flex justify-evenly gap-12 cursor-pointer  ">
             <li>
               <a href="#home">Home</a>
             </li>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,12 +3,14 @@ import { servicesCon } from "../assets/assets";
 import { motion } from "motion/react";
 const Project = () => {
   return (
-    <motion.div initial={{ y:100}} transition={{delay:0.2 ,duration:1}} whileInView={{y:0}} viewport={{once:true}}
+    <div 
      className=" flex flex-col items-center justify-center
      bg-pink-50/50 py-10 px-10 ">
-    <h1 className="text-4xl font-semibold py-4">Projects</h1>
-    <hr className="border-t-2 border-red-600 w-32 my-2 " />
-      <motion.div initial={{ y:100}} transition={{delay:0.6,duration:1}} whileInView={{y:0}} viewport={{once:true}}
+    <motion.h1 initial={{ y:100}} transition={{delay:0.2 ,duration:1}} whileInView={{y:0}} viewport={{once:true}}
+     className="text-4xl font-semibold py-4">Projects</motion.h1>
+    <motion.hr  initial={{ y:100}} transition={{delay:0.2 ,duration:1}} whileInView={{y:0}} viewport={{once:true}}
+     className="border-t-2 border-red-600 w-32 my-2 " />
+      <motion.div initial={{ y:100}} transition={{delay:0.2,duration:1}} whileInView={{y:0}} viewport={{once:true}}
        className=" flex  gap-10 py-7">
         {servicesCon.map((item, i) => (
           <div key={i} className="flex flex-col justify-center items-center gap-5">
@@ -18,7 +20,7 @@ const Project = () => {
           </div>
         ))}
       </motion.div>
-    </motion.div>
+    </div>
   );
 };
 
